b1bc4438c5202a9cdd68a2f2e669a8f22c8322ef
rafael <rafaelsistem87@gmail.com>
2021-08-29 05:05:31 -0700

    first commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        this.app.get('/api', (req, res)=>{
+              res.send('hello word')
+        });
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
