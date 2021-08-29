c7bf013302242dad41077be18c71e5f286405eb8
rafael <rafaelsistem87@gmail.com>
2021-08-29 05:21:40 -0700

    identation

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-const Server = require('./models/server');
+require('dotenv').config()
+const Server = require('./models/server')
 
+const server = new Server()
 
-const server = new Server();
-
-
-
-server.listen();
+server.listen()
